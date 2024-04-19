1e0a8603d6fc9a725546344a828b1c31ec22fd37
amar-and-stuff <superbumper000@gmail.com>
2024-04-19 19:29:38 +0530

    hr add_employee form error resolved

--- src/main/java/com/empsys/ems/hr/HRController.java
+++ src/main/java/com/empsys/ems/hr/HRController.java
@@ -5,10 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.empsys.ems.employee.Employee;
+import com.empsys.ems.manager.Manager;
+
 
 
 @Controller
@@ -31,15 +33,27 @@ public class HRController {
         return "hr_templates/dashboard";
     }
 
+    @PostMapping("/add_employee")
+    String submitEmployee(@RequestParam int id, @RequestParam String name, @RequestParam String designation, @RequestParam int salary, @RequestParam float exp) {
+        //hr added an employee
+        Employee emp;
+        if (designation.equals("Employee")) {
+            emp = new Employee(id, name, designation, salary, exp);
+        } else if (designation.equals("HR")) {
+            emp = new HR(id, name, designation, salary, exp);
+        } else if (designation.equals("manager")) {
+            emp = new Manager(id, name, designation, salary, exp);
+        }
+        
+        return "hr_templates/add_employee_template";
+    }
+
     @GetMapping("/add_employee")
     String addEmployee() {
         return "hr_templates/add_employee_template";
     }
 
-    @PostMapping("/submit_employee")
-    String submitEmployee(@RequestBody Employee employee) {
-        return "hr_templates/add_employee_template";
-    }
+    
 
     @GetMapping("/logout")
     String logout() {
