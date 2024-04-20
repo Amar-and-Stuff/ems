d129ca0c3d3b2d3270d42df098bfcf8aff82ad72
Mounish <mounishmaram2002@gmail.com>
2024-04-20 11:37:22 +0530

    Added Management Page

--- src/main/java/com/empsys/ems/manager/ManagerRepository.java
+++ src/main/java/com/empsys/ems/manager/ManagerRepository.java
@@ -3,8 +3,11 @@ package com.empsys.ems.manager;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ManagerRepository {
-    public List<Manager> managers;
+    public ArrayList<Manager> managers;
     ManagerRepository() {
         managers=new ArrayList<>();
         managers.add(new Manager(1, "Mounish", "Employee", 1000, 0));
@@ -14,6 +17,9 @@ public class ManagerRepository {
         managers.add(new Manager(5, "Karthik", "Employee", 1000, 0));
     }   
     public Manager getManagerById(int id) {
-        return managers.stream().filter(emp->emp.getID()==id).findFirst().get();
+        return managers.stream().filter(emp->emp.getId()==id).findFirst().get();
     } 
+    public ArrayList<Manager> getList() {
+        return managers;
+    }
 }
