bc3e14cf6bf74b5e8a6dcefa20b80cd2ddd3831b
Mounish <mounishmaram2002@gmail.com>
2024-04-20 18:46:08 +0530

    Delete functionality added

--- src/main/java/com/empsys/ems/manager/ManagerController.java
+++ src/main/java/com/empsys/ems/manager/ManagerController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -64,9 +63,11 @@ public class ManagerController {
             managerRepository.updateEmployeeById(employee_id, employee);
             return "manager_templates/update_employee";
         }
-    @DeleteMapping("/{id}")
-    void deleteEmployee(@PathVariable int employee_id) {
+    @GetMapping("/manage/delete/{manager_id}/")
+    String deleteEmployee(@PathVariable int manager_id,@RequestParam(name="employee_id",required=true) int employee_id,Model model) {
         managerRepository.deleteEmployeeById(employee_id);
+        model.addAttribute("manager_id", manager_id);
+        return "redirect:/manager/manage/"+manager_id;
     }
     @GetMapping("/logout")
     String logout() {
