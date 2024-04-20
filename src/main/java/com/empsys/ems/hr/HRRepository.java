81ed434e3a595a0d6b99bc489c15a9c67751aa50
amar-and-stuff <superbumper000@gmail.com>
2024-04-20 21:11:34 +0530

    removed list comments

--- src/main/java/com/empsys/ems/hr/HRRepository.java
+++ src/main/java/com/empsys/ems/hr/HRRepository.java
@@ -1,6 +1,5 @@
 package com.empsys.ems.hr;
 
-// import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.jdbc.core.simple.JdbcClient;
@@ -10,20 +9,10 @@ import com.empsys.ems.employee.Employee;
 
 @Repository
 public class HRRepository {
-    // private List<HR> hrs;
     private JdbcClient jdbcClient;
     HRRepository(JdbcClient jdbcClient) {
         this.jdbcClient = jdbcClient;
-        // hrs=new ArrayList<>();
-        // hrs.add(new HR(1, "Mounish", "HR", 1000, 0));
-        // hrs.add(new HR(2, "Amar", "HR", 1000, 0));
-        // hrs.add(new HR(3, "Kiran", "HR", 1000, 0));
-        // hrs.add(new HR(4, "Dhanush", "HR", 1000, 0));
-        // hrs.add(new HR(5, "Karthik", "HR", 1000, 0));
     }
-    // public HR getHRByIdListVersion(int id) {
-    //     return hrs.stream().filter(emp->emp.getId()==id).findFirst().get();
-    // }
 
     public Employee getEmployeeById(int id) {
         return jdbcClient.sql("SELECT id, name, designation, salary, exp  FROM employees WHERE id=:id")
--- src/main/java/com/empsys/ems/manager/ManagerRepository.java
+++ src/main/java/com/empsys/ems/manager/ManagerRepository.java
@@ -1,6 +1,5 @@
 package com.empsys.ems.manager;
 
-// import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.jdbc.core.simple.JdbcClient;
@@ -10,20 +9,10 @@ import com.empsys.ems.employee.Employee;
 
 @Repository
 public class ManagerRepository {
-    // public List<Manager> managers;
     private JdbcClient jdbcClient;
     ManagerRepository(JdbcClient jdbcClient) {
         this.jdbcClient = jdbcClient;
-        // managers=new ArrayList<>();
-        // managers.add(new Manager(1, "Mounish", "Employee", 1000, 0));
-        // managers.add(new Manager(2, "Amar", "Employee", 1000, 0));
-        // managers.add(new Manager(3, "Kiran", "Employee", 1000, 0));
-        // managers.add(new Manager(4, "Dhanush", "Employee", 1000, 0));
-        // managers.add(new Manager(5, "Karthik", "Employee", 1000, 0));
-    }   
-    // public Manager getManagerByIdListVersion(int id) {
-    //     return managers.stream().filter(emp->emp.getId()==id).findFirst().get();
-    // } 
+    }
 
     public Employee getEmployeeById(int id) {
         return jdbcClient.sql("SELECT id, name, designation, salary, exp  FROM employees WHERE id=:id")
